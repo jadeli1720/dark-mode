57ef01017cac2e2ba809ba12fab3b7fb73c20f9e
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-25 15:14:19 -0600

    Added setValue function

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,14 +1,21 @@
 import { useState } from "react";
 
+//Uncomment so that you can see what is going on in console.
+
 export default function useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
-        console.log(`useLocalStorage:useState`);
+        // console.log(`useLocalStorage:useState`);
 
         const item = window.localStorage.getItem(key);
-        console.log(`useLocalStorage:useState`, key, initialValue, item);
+        // console.log(`useLocalStorage:useState`, key, initialValue, item);
 
         return item ? JSON.parse(item) : initialValue
     });
+    const setValue = value => {
+        setStoredValue(value);
+        // console.log(`useLocalStorage:useState:`, value);
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }
 
-    return [storedValue];
+    return [storedValue, setValue];
 };
