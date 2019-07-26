803d86d55b4fb7a086a8788d8b2b1253803cde06
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-25 20:36:33 -0600

    Writing notes to further understanding of localStorage and custom hooks

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+//Don't need to import React from react. Will not be using it!
 
 //Uncomment so that you can see what is going on in console.
 
@@ -9,7 +10,15 @@ export const useLocalStorage =(key, initialValue) => {
         const item = window.localStorage.getItem(key);
         // console.log(`useLocalStorage:useState`, key, initialValue, item);
 
-        return item ? JSON.parse(item) : initialValue
+        //If statement that does the same as the Ternary
+        if (item) {
+            return JSON.parse(item);
+        } else {
+            return initialValue;
+        }
+
+        //Ternary operator below. It is a expression the returns true or false.     true branch    :  false branch
+        // return item ? JSON.parse(item) : initialValue
     });
     const setValue = value => {
         setStoredValue(value);
@@ -18,4 +27,7 @@ export const useLocalStorage =(key, initialValue) => {
     }
 
     return [storedValue, setValue];
+    //storedValue = the value or variable you start with
+    // setValue= the function used to change storedValue with.
+    //This is just like the regular useState that is already set up in this way, we are CUSTOMIZING the function part of the hook.
 };
